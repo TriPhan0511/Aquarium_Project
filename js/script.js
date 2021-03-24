97760cc813a9a308ec590b88302b5118d8604830
Tri Phan <phanphutri@gmail.com>
2021-03-24 14:19:39 +0700

    Big Updated

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// var toolTipElement = document.getElementById('tooltip');
+// console.log(toolTipElement);
+
+// let navbars = document.querySelectorAll('.navbar');
+// console.log(navbars);
+
+let items = document.querySelectorAll('.dropdown');
+// console.log(items);
+// console.log(items[0]);
+
+// let myItem = items[0];
+// console.log(myItem);
+
+// myItem.addEventListener('click', greeting);
+
+function greeting() {
+    console.log('Hello, world');
+}
+
+// let myLink = document.querySelector('#dropdown03');
+// console.log(myLink);
+
+// // myLink.addEventListener('click', greeting);
+
+// myLink.onclick = function () {
+//     alert('hello');
+//     console.log('hello');
+// };
+
+let activeLinks = document.querySelectorAll('.active');
+console.log(activeLinks);
+
+let firstActiveLink = activeLinks[0];
+console.log(firstActiveLink);
+
+// firstActiveLink.addEventListener('hover', greeting);
+firstActiveLink.onmouseover = function () {
+    console.log('Hello, world');
+};
